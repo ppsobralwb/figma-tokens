f5a09de73f6e372715964d07e9369e817c046884
Pedro Paulo Moraes <pedropaulo.moraes@technipfmc.com>
2023-01-04 10:16:21 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 03 Jan 2023 18:13:14 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#646565",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#825cf6"
+};
